6b783fefc7074a809e58a9ba743592e976b8da37
Kaktotaks <leonidshevchenko350@gmail.com>
2021-09-30 16:23:01 +0300

    segmentChange added

--- DailyMail/Views/MediaViewController.swift
+++ DailyMail/Views/MediaViewController.swift
@@ -12,17 +12,20 @@ import RealmSwift
 class MediaViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var mediaSegmentedControl: UISegmentedControl!
     
     var mostVieweds: [Viewed] = []
-    var viewedMediaMetadatas: [ViewedMediaMetadata] = []
+//    var viewedMediaMetadatas: [ViewedMediaMetadata] = []
+    var mostEmaileds: [Emailed] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "DailyMail"
         
-//        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MostViewedTableViewCell")
         self.tableView.register(UINib(nibName: "MostViewedTableViewCell", bundle: nil), forCellReuseIdentifier: "MostViewedTableViewCell")
+        
+        self.tableView.register(UINib(nibName: "MostEmailedTableViewCell", bundle: nil), forCellReuseIdentifier: "MostEmailedTableViewCell")
         
     }
     
@@ -30,6 +33,7 @@ class MediaViewController: UIViewController {
         super.viewWillAppear(animated)
         
         self.requestMostViewed()
+        self.requestMostEmailed()
         
     }
     
@@ -45,24 +49,70 @@ class MediaViewController: UIViewController {
             }
         }
     }
+    
+    func requestMostEmailed() {
+        let url = "https://api.nytimes.com/svc/mostpopular/v2/emailed/30.json?api-key=vuIfURUfUeJfm6S5iYK17hxXNBPBOQEz"
+        AF.request(url).responseJSON { responce in
+            
+            let decoder = JSONDecoder()
+            
+            if let data = try? decoder.decode(EmailedResult.self, from: responce.data!){
+                self.mostEmaileds = data.emaileds ?? []
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    
 }
 
 extension MediaViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.mostVieweds.count
+        
+        let selectedIndex = self.mediaSegmentedControl.selectedSegmentIndex
+        
+        switch selectedIndex
+        {
+        case 0:
+            return self.mostVieweds.count
+        case 1:
+            return self.mostEmaileds.count
+        default:
+            return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let mostViewedCell = tableView.dequeueReusableCell(withIdentifier: "MostViewedTableViewCell", for: indexPath) as? MostViewedTableViewCell else {
+        let selectedIndex = self.mediaSegmentedControl.selectedSegmentIndex
+        switch selectedIndex {
+            
+        case 0:
+            guard let mostViewedCell = tableView.dequeueReusableCell(withIdentifier: "MostViewedTableViewCell", for: indexPath) as? MostViewedTableViewCell else {
+                return UITableViewCell()
+            }
+            let mostViewedMedia = self.mostVieweds[indexPath.row]
+//            let mostViewedPathString = self.mostVieweds[indexPath.row].url ?? ""
+            mostViewedCell.mostViewedConfigureWith(imageURL: URL(string: ""), mostViewedName: mostViewedMedia.title, publishedDateText: mostViewedMedia.published_date)
+            return mostViewedCell
+            
+        case 1:
+            guard let mostEmailedCell = tableView.dequeueReusableCell(withIdentifier: "MostEmailedTableViewCell", for: indexPath) as? MostEmailedTableViewCell else {
+                return UITableViewCell()
+            }
+            let mostEmailedMedia = self.mostEmaileds[indexPath.row]
+//            let mostEmailedPathString = self.mostEmaileds[indexPath.row].url ?? ""
+            mostEmailedCell.mostEmailedConfigureWith(imageURL: URL(string: ""), mostEmailedName: mostEmailedMedia.title, publishedDateText: mostEmailedMedia.published_date)
+            return mostEmailedCell
+            
+        default:
             return UITableViewCell()
         }
-        let mostViewedMedia = self.mostVieweds[indexPath.row]
-        let mostViewedPathString = self.mostVieweds[indexPath.row].url ?? ""
-        mostViewedCell.mostViewedConfigureWith(imageURL: URL(string: mostViewedPathString), mostViewedName: mostViewedMedia.title, publishedDateText: mostViewedMedia.published_date)
-        return mostViewedCell
     }
     
+    @IBAction func mediaSegmentedChanged(_ sender: UISegmentedControl) {
+        self.tableView.reloadData()
+    }
 }
 
 // MARK: - Save !
